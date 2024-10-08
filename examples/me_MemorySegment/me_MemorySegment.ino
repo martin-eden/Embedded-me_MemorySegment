2085f312cb9e7c1b0b4f61d34141b6640e73a261
Ian <ian@somewhere.com>
2024-10-09 00:37:07 +0200

    [/] [Example] Switched to using [me_Console]..
    Got formatted output but code is cluttered with "Console.".
    Anyway, free function for debug printing uses [me_Console].
    So example should have too.

--- examples/me_MemorySegment/me_MemorySegment.ino
+++ examples/me_MemorySegment/me_MemorySegment.ino
@@ -2,23 +2,22 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-10-05
+  Last mod.: 2024-10-08
 */
 
 #include <me_MemorySegment.h>
 
-#include <me_UartSpeeds.h>
-#include <me_InstallStandardStreams.h>
 #include <me_BaseTypes.h>
+#include <me_UartSpeeds.h>
+#include <me_Console.h>
 
 void setup()
 {
-  Serial.begin(me_UartSpeeds::Arduino_Normal_Bps);
-  InstallStandardStreams();
+  Console.Init(me_UartSpeeds::Arduino_Normal_Bps);
 
-  printf("[me_MemorySegment] Okay, we are here.\n");
+  Console.Print("[me_MemorySegment] Okay, we are here.");
   RunTest();
-  printf("[me_MemorySegment] Done.\n");
+  Console.Print("[me_MemorySegment] Done.");
 }
 
 void loop()
@@ -29,46 +28,70 @@ void loop()
 
 void RunTest()
 {
-  printf(
-    "This demo will show typical applications of [me_MemorySegment].\n"
+  /*
+    Guys, sorry for example cluttered with pretty-printing.
+  */
+
+  Console.Line();
+  Console.Write(
+    "This demo will show typical applications of [me_MemorySegment]\n"
     "We recommend to match output with reading code.\n"
-    "\n"
   );
+  Console.Indent();
 
   using
     me_MemorySegment::TMemorySegment,
     me_MemorySegment::Freetown::FromAsciiz,
-    me_MemorySegment::Freetown::Print,
     me_MemorySegment::Freetown::PrintWrappings;
 
   TMemorySegment MemSeg;
 
-  printf("Casting ASCIIZ to memory segment:\n");
+  Console.Print("Getting memory segment from ASCIIZ");
 
   // Treat ASCIIZ as memory segment. Just cast, no data copied.
   MemSeg = FromAsciiz("ASCIIZ as memory segment. Goodbye zero tail!");
 
-  printf("Contents( ");
-  Print(MemSeg);
-  printf(" )\n");
-  printf("\n");
+  Console.Indent();
+  Console.Write("Contents ( ");
+  Console.Write(MemSeg);
+  Console.Write(" )");
+  Console.Newline();
+  Console.Unindent();
 
-  printf("Casting AVR registers to memory segment:\n");
+  Console.Indent();
+  Console.Write("Debug contents ( ");
+  Console.Indent();
+  PrintWrappings(MemSeg);
+  Console.Unindent();
+  Console.Write(")");
+  Console.Newline();
+  Console.Unindent();
+
+  Console.Line();
+
+  Console.Print("Getting memory segment from AVR registers");
 
   // AVR registers are at addresses 0 .. 31
   MemSeg.Start.Addr = 0;
   MemSeg.Size = 32;
 
-  printf("Debug contents(\n");
-  PrintWrappings(&MemSeg);
-  printf(")\n");
-  printf("\n");
+  Console.Indent();
+  Console.Write("Debug contents ( ");
+  Console.Indent();
+  PrintWrappings(MemSeg);
+  Console.Unindent();
+  Console.Write(")");
+  Console.Newline();
+  Console.Unindent();
 
-  printf(
+  Console.Unindent();
+
+  Console.Write(
+    "\n"
     ".. also there are intersection and equality checking functions.\n"
     "But we are too lazy to write demo code for them.\n"
-    "\n"
   );
+  Console.Line();
 }
 
 /*
@@ -78,4 +101,5 @@ void RunTest()
   2024-05-29
   2024-06-02
   2024-10-05
+  2024-10-08
 */
